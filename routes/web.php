22e75debeebcba2d8b037a89ac29f31bad166cb1
theizekry <theizekry@gmail.com>
2025-10-18 07:05:28 +0300

    Init Commit - Enabled Xdebug with docker and phpstorm.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
+    $x = 'sdsd';
+
     return response()->json([
         'message' => 'Welcome to Horizon One.',
         'version' => '1.0.0',
